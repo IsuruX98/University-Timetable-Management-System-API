1337e07dac14e15c0edfb4608ae60e24bbd5238c
IsuruX98 <isurusanka98@gmail.com>
2024-03-05 00:29:26 +0530

    basic structure created

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Database Connection
+const connectDB = require("./config/db");
+connectDB();
+
+// Routes
+
+// Start the Server
+const server = app.listen(port, () =>
+  console.log(`Server running on port ${port} 🔥`)
+);
